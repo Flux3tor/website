105157f6a71c0ea0b2801e36c5cff629348df317
Tushar Sinha <tusharsinha199@gmail.com>
2026-02-07 20:37:43 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,1 +1,12 @@
+const searchInput = document.getElementById("project-search");
 
+searchInput.addEventListener("input", () => {
+  const term = searchInput.value.toLowerCase();
+
+  const cards = document.querySelectorAll(".project-card");
+
+  cards.forEach(card => {
+    const text = card.innerText.toLowerCase();
+    card.style.display = text.includes(term) ? "flex" : "none";
+  });
+});
