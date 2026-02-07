d5de03f020b952106b31bd0345b7468652f9a718
Tushar Sinha <tusharsinha199@gmail.com>
2026-02-07 20:41:30 +0530

    Update easterEggs.js

--- js/easterEggs.js
+++ js/easterEggs.js
@@ -1,1 +1,13 @@
+console.log("👀 curious dev, huh?");
+console.log("Try Ctrl + K");
 
+const logo = document.querySelector(".logo");
+let clicks = 0;
+
+logo.addEventListener("click", () => {
+  clicks++;
+  if (clicks === 5) {
+    alert("Dev mode unlocked.");
+    clicks = 0;
+  }
+});
