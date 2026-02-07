396b0060604c5be5f968c4953f85b2aec9275c85
Tushar Sinha <tusharsinha199@gmail.com>
2026-02-07 20:40:45 +0530

    Update shortcuts.js

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -1,1 +1,19 @@
+document.addEventListener("keydown", (e) => {
+  if (e.target.tagName === "INPUT") return;
 
+  if (e.key.toLowerCase() === "p") {
+    document.getElementById("projects").scrollIntoView();
+  }
+
+  if (e.key.toLowerCase() === "t") {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  if (e.key.toLowerCase() === "n") {
+    document.getElementById("now").scrollIntoView();
+  }
+
+  if (e.key.toLowerCase() === "c") {
+    document.getElementById("copy-email").click();
+  }
+});
